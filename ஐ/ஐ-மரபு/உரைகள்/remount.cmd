8062885265ca6b04bb02f62bc09a45a8cdfe219c
Anish T <anish.t@ni.com>
2021-01-24 16:21:00 +0530

    தமிழ்
    தமிழ்

---
+++ ஐ/ஐ-மரபு/உரைகள்/remount.cmd
@@ -0,0 +1,10 @@
+﻿rem http://tamilneram.github.io
+@echo off
+cls
+
+ Set Distroname=ISONAMESLUG
+ echo %Distroname%
+ subst Z: "%~d0\!\%Distroname%"
+ echo mounted "%~d0\!\%Distroname%" as Z:
+ REM pause
+exit
